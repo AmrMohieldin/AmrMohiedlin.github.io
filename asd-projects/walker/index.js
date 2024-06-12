5e28ff1615cf569f5df7effac46e9b194abee915
Amr Mohieldin <amr.mohieldin@jpschools.org>
2024-06-12 16:03:26 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -18,6 +18,10 @@ function runProgram(){
     UP: 38,
     RIGHT: 39,
     DOWN: 40,
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68
   };
 
   var walker = {
@@ -28,10 +32,50 @@ function runProgram(){
     width: 50,
   }
 
+  var walker1 = {
+    posX: $("#board").width() - walker.width,
+    posY: 0,
+    speedX: 0,
+    speedY: 0,
+    width: 50,
+  }
+  // Code to make a random color
+  var colors = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+  var colr = "#"
+  var colr1 = "#"
+  
+  function getRandomIntInc(min, max) {
+    const minCei = Math.ceil(min);
+    const maxFlo = Math.floor(max);
+    return Math.floor(Math.random() * (maxFlo - minCei + 1) + minCei)};
+
+
+  function changeColor(){
+    colr = ""
+    for (var i = 0; i < 6; i++){
+      var hi = getRandomIntInc(0, 16)
+      colr += colors[hi]
+      }
+    $("#walker1").css("background-color", colr); }
+  
+  function changeColor1(){
+    colr1 = ""
+    for (let i = 0; i < 6; i++){
+      var hi1 = getRandomIntInc(0, 16)
+      colr1 += colors[hi1]
+      
+      }
+    $("#walker").css("background-color", colr1); }
+
+  
+
+
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           
   $(document).on('keyup', handleKeyUp);
+  walker1.on('click', changeColor);
+  walker.on('click', changeColor1);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -63,12 +107,27 @@ function runProgram(){
     else if (event.which === KEY.RIGHT) {
       walker.speedX = 5;
     }
+    else if (event.which === KEY.W) {
+      walker1.speedY = -5;
+    }
+    else if (event.which === KEY.S) {
+      walker1.speedY = 5;
+    }
+    else if (event.which === KEY.A) {
+      walker1.speedX = -5;
+    }
+    else if (event.which === KEY.D) {
+      walker1.speedX = 5;
+    }
+    
     
   }
 
   function handleKeyUp(event) {
     walker.speedX = 0;
     walker.speedY = 0;
+    walker1.speedX = 0;
+    walker1.speedY = 0;
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -87,13 +146,20 @@ function runProgram(){
   function repositionGameItem() {
     walker.posX += walker.speedX;  // update the position of the box along the x-axis
     walker.posY += walker.speedY;  // update the position of the box along the y-axis
+    walker1.posX += walker1.speedX;  // update the position of the box along the x-axis
+    walker1.posY += walker1.speedY;  // update the position of the box along the y-axis
+
 
   }
 
   function redrawGameItem() {
     $("#walker").css("left", walker.posX); // draw the box in the new location, positionX pixels away from the "left"
     $("#walker").css("top", walker.posY); // draw the box in the new location, positionY pixels away from the "top"
+    $("#walker1").css("left", walker1.posX); // draw the box in the new location, positionX pixels away from the "left"
+    $("#walker1").css("top", walker1.posY); // draw the box in the new location, positionY pixels away from the "top"
   }
+
+  
 
  
   function wallCollision() {
@@ -102,6 +168,12 @@ function runProgram(){
     }
     if (walker.posY > $("#board").height() - walker.width || walker.posY < 0){
       walker.posY -= walker.speedY
+    }
+    if (walker1.posX > $("#board").width() - walker.width || walker1.posX < 0){
+      walker1.posX -= walker1.speedX
+    }
+    if (walker1.posY > $("#board").height() - walker.width || walker1.posY < 0){
+      walker1.posY -= walker1.speedY
     }
   }
   
