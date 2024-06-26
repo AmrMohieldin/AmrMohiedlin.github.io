ee02e70d548f16d034d7aafb54b4a1932669a6c5
Amr Mohieldin <amr.mohieldin@jpschools.org>
2024-06-26 15:35:00 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -13,28 +13,33 @@ function runProgram(){
   
   // Game Item Objects
   var paddle1 = {};
-  paddle1.x = 20;
-  paddle1.y = 180;
+  paddle1.posX = 20;
+  paddle1.posY = 180;
   paddle1.width = 20;
   paddle1.height = 80;
-  paddle1.speedY = 1;
+  paddle1.speedY = 0;
 
   var paddle2 = {};
-  paddle2.x = 400;
-  paddle2.y = 180;
+  paddle2.posX = 400;
+  paddle2.posY = 180;
   paddle2.width = 20;
   paddle2.height = 80;
-  paddle2.speedY = 1;
+  paddle2.speedY = 0;
+
+  var ballSpeed = (Math.random() * 3 + 2) 
+  var ballSpeedY = (Math.random() * 2 + 1) * (Math.random() > 0.5 ? -1 : 1);
 
   var ball = {};
-  ball.x = 20;
+  ball.x = 80;
   ball.y = 180;
   ball.width = 20;
   ball.height = 80;
-  ball.speedX = 1;
-  ball.speedY = 1;
+  ball.speedX = ballSpeed;
+  ball.speedY = ballSpeedY;
   ball.id = "#ball";
 
+  var score1 = 0;
+  var score2 = 0;
 
   var KEY = {
     UP: 38,
@@ -63,6 +68,7 @@ function runProgram(){
     repositionGameItem()
     redrawGameItem()
     wallCollision()
+    checkWhoWon()
   }
   
   /* 
@@ -73,16 +79,16 @@ function runProgram(){
 
   function handleEvent(event) {
     if (event.which === KEY.UP) {
-      paddle1.speedY = -5;
-    }
-    else if (event.which === KEY.DOWN) {
-      paddle1.speedY = 5;
-    }
-    else if (event.which === KEY.W) {
       paddle2.speedY = -5;
     }
-    else if (event.which === KEY.S) {
+    else if (event.which === KEY.DOWN) {
       paddle2.speedY = 5;
+    }
+    else if (event.which === KEY.W) {
+      paddle1.speedY = 5;
+    }
+    else if (event.which === KEY.S) {
+      paddle1.speedY = -5;
     }}
 
     function handleKeyUp(event) {
@@ -97,29 +103,59 @@ function runProgram(){
       ////////////////////////////////////////////////////////////////////////////////
 
     function wallCollision() {
-      if (paddle1.posY > $("#board").height() - paddle1.width || paddle1.posY < 0){
+      if (paddle1.posY > $("#board").height() - paddle1.height || paddle1.posY < 0){
         paddle1.posY -= paddle1.speedY
       }
-      if (paddle2.posY > $("#board").height() - paddle2.width || paddle2.posY < 0){
+      if (paddle2.posY > $("#board").height() - paddle2.height || paddle2.posY < 0){
         paddle2.posY -= paddle2.speedY
       }
-    }
+      if (ball.y > $("#board").height() - 20 || ball.y < 0){
+        ball.speedY *= -1
+      }
+      if (ball.x > $("#board").width() - 20){
+        ball.x = 220
+        ball.y = 220
+        score1 += 1
+        $("#score1").text("Score: " + score1)}
+        
+        if (ball.x < 0){
+        ball.x = 220
+        ball.y = 220
+        score2 += 1
+        $("#score2").text("Score: " + score2)}
+    
+      if (ball.x - 10 < 20 && paddle1.posY <= ball.y && ball.y >= paddle1.posY + 40 || ball.x + 10 >= paddle2.posX && paddle2.posY <= ball.y && ball.y >= paddle2.posY + 40) {
+           ball.speedX *= -1
+           ball.speedY *= -1
+      }}
+      
+    
   
 
   function repositionGameItem() {
 
     paddle1.posY += paddle1.speedY;  // update the position of the box along the y-axis
     paddle2.posY += paddle2.speedY;  // update the position of the box along the y-axis
+    ball.x += ball.speedX;
+    ball.y += ball.speedY; 
   }
 
   function redrawGameItem() {
     $("#paddle1").css("top", paddle1.posY); // draw the box in the new location, positionY pixels away from the "top"
-    $("#paddle2").css("top", paddle1.posY); // draw the box in the new location, positionY pixels away from the "top"
+    $("#paddle2").css("top", paddle2.posY); // draw the box in the new location, positionY pixels away from the "top"
+    $("#ball").css("top", ball.y); // draw the box in the new location, positionY pixels away from the "top"
+    $("#ball").css("left", ball.x); // draw the box in the new location, positionY pixels away from the "left"
   }
 
-  
-
-
+  function checkWhoWon(){
+    if (score1 >= 11){
+      $("#winner").text("Player 1 wins!")
+      endGame()}
+    if (score2 >= 11){
+      $("#winner").text("Player 2 wins!")
+      endGame()}
+    
+  }  
   
   function endGame() {
     // stop the interval timer
@@ -127,6 +163,10 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+
+    
   }
-  
+ 
+
+
 }
